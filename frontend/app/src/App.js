d406d3de0f9691cc1bc2f98e42e5d9539264c30e
Anthony Yang <ay339@cornell.edu>
2019-05-23 12:21:26 -0400

    Added material ui

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
 import './App.css';
 
 function App() {
@@ -19,6 +23,16 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="title" color="inherit">
+              React &amp; Material-UI Sample Application
+                </Typography>
+          </Toolbar>
+          <Button>Hello World</Button>
+        </AppBar>
+      </div>
     </div>
   );
 }
