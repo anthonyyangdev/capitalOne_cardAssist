f41fd58907f805c5270eec8e5491a807ca1ffc31
wendywu830 <44139775+wendywu830@users.noreply.github.com>
2019-05-23 14:48:38 -0400

    Added home page

---
+++ frontend/app/Home.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+
+  render() {
+    return(
+      <div>
+        <h1>Welcome to Card Assist!</h1>
+        <h2>How can we help?</h2>
+      </div>
+      
+    )
+  }
+
+}
+
+export default Home
