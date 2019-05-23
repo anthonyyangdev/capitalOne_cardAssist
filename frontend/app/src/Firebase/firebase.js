483cc2ea20a9b745b17a6bbe9c14f001182fd146
Braden Goodwin <goodwin.braden@gmail.com>
2019-05-23 14:24:18 -0400

    installed npm and set up firebase

---
+++ frontend/app/src/Firebase/firebase.js
@@ -0,0 +1,19 @@
+import app from 'firebase/app'
+
+const config = {
+  apiKey: "AIzaSyBX9gu9YMYSVMM6YWEc3-q8a9WLw-TgtRI",
+  authDomain: "creditcardresearch-19e6c.firebaseapp.com",
+  databaseURL: "https://creditcardresearch-19e6c.firebaseio.com",
+  projectId: "creditcardresearch-19e6c",
+  storageBucket: "creditcardresearch-19e6c.appspot.com",
+  messagingSenderId: "150007916872",
+  appId: "1:150007916872:web:eeb6760dd602e3de"
+};
+
+class Firebase{
+  constructor() {
+    app.initializeApp(config);
+  }
+}
+
+export default Firebase;
